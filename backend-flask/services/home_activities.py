38027f1ed380f3f79c8514554f6b5b7d4457f2fa
Daniel Alvarez <alvarez.daniel@gmail.com>
2023-03-16 15:51:56 +0000

    add db to home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from lib.db import pool, query_wrap_array
 
 tracer = trace.get_tracer("home.activities")
 
@@ -14,6 +15,7 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       
+      """ no more hardcoded data
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -67,7 +69,34 @@ class HomeActivities:
         }
         
         results.insert(0,extra_crud)
-
+      
       span.set_attribute("app.result_length", len(results))
       
-      return results
+      return results      
+      """
+
+      sql = query_wrap_array("""
+        SELECT
+          activities.uuid,
+          users.display_name,
+          users.handle,
+          activities.message,
+          activities.replies_count,
+          activities.reposts_count,
+          activities.likes_count,
+          activities.reply_to_activity_uuid,
+          activities.expires_at,
+          activities.created_at
+        FROM public.activities
+        LEFT JOIN public.users ON users.uuid = activities.user_uuid
+        ORDER BY activities.created_at DESC
+      """)
+      print("SQL--------------")
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
